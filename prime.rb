463ce30ca0cdc5952f7751203d3618ce11b67c43
Jared S <jaredjames2020@gmail.com>
2019-09-21 12:58:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,7 @@
 require 'pry'
 # Add  code here!
 def prime?(number)
-  ((number - 1)..2).each do |n|
-    #binding.pry
+  (2..(number - 1)).each do |n|
     return false if number % n == 0 || n < 0
   end
   true
