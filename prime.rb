e2faf506d37c317e3aa465b3de635f760469a980
Jared S <jaredjames2020@gmail.com>
2019-09-21 12:46:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,7 @@
-require 'prime'
-
 # Add  code here!
 def prime?(number)
   (2..(number - 1)).each do |n|
-    return false if number % n == 0
+    return false if number % n == 0 || n < 0
   end
   true
 end
