75fcb80c2322d1d4b60d403265d3f8c259b4e6d8
Jared S <jaredjames2020@gmail.com>
2019-09-21 12:40:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,11 @@ require 'prime'
 
 # Add  code here!
 def prime?(number)
+  (2..(number - 1)).each do |n|  
 end
 
 def is_prime(num)
-  (2..(num - 1)).each do |n|
+
     return false if num % n == 0
   end
   true
